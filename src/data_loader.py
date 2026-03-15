98e3a29b7b074bc5ea7dbd086f05c06f07307155
Kamil Szulim <kszulim668@gmail.com>
2026-03-15 23:12:06 +0100

    first returns calculation

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,2 +1,12 @@
 import yfinance as yf
 
+def download_prices(tickers, start='2015-01-01'):
+
+    data = yf.download(
+        tickers,
+        start=start
+    )
+
+    prices = data["Close"]
+
+    return prices
